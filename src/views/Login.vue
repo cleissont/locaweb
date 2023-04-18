1f13f38f1392cf52521acd2f450b5a7a35549ad2
Cleisson <cleissontrindade@hotmail.com>
2023-04-17 21:36:04 -0300

    Project structure

---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <span>teste</span>
+  </div>
+</template>
+<script>
+  export default {}
+</script>
+<style lang=""></style>
---
+++ src/views/Register.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div></div>
+</template>
+<script>
+  export default {}
+</script>
+<style lang=""></style>
